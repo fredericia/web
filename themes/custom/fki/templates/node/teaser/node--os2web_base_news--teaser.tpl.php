4fd131d6bd8ba665a9680d0d95a93537ead9a20f
Morten Nissen <mail@hjemmesidekongen.dk>
2016-05-09 06:43:51 +0200

    News teaser

--- themes/custom/fki/templates/node/teaser/node--os2web_base_news--teaser.tpl.php
+++ themes/custom/fki/templates/node/teaser/node--os2web_base_news--teaser.tpl.php
@@ -1,24 +1,24 @@
 <?php if ($view_mode == 'teaser'): ?>
   <!-- node--os2web_base_news--teaser.tpl.php -->
   <!-- Begin - teaser -->
-  <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> os2-node-teaser os2-news-teaser"<?php print $attributes; ?>>
-
-    <?php if ($updated_at_short): ?>
-      <div class="os2-node-teaser-date">
-        <?php print $created_at_short; ?>
-      </div>
-    <?php endif; ?>
-
-    <?php if ($title): ?>
-      <!-- Begin - heading -->
-      <div class="os2-node-teaser-heading">
-        <h3 class="os2-node-teaser-heading-title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h3>
-      </div>
-      <!-- End - heading -->
-    <?php endif; ?>
+  <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> os2-box os2-node-teaser os2-news-teaser"<?php print $attributes; ?>>
 
     <!-- Begin - body -->
-    <div class="os2-node-teaser-body">
+    <div class="os2-node-teaser-body os2-box-body">
+
+      <?php if ($updated_at_short): ?>
+        <div class="os2-node-teaser-date">
+          <?php print $created_at_short; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($title): ?>
+        <!-- Begin - heading -->
+        <div class="os2-node-teaser-body-heading">
+          <h3 class="os2-node-teaser-body-heading-title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h3>
+        </div>
+        <!-- End - heading -->
+      <?php endif; ?>
 
       <?php if (isset($content['field_os2web_base_field_summary'])): ?>
         <!-- Begin - summary -->
