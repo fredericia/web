95dd665b7c48437a27cce55066042b943876f3ab
Morten Nissen <mn@bellcom.dk>
2016-05-29 13:50:52 +0200

    Improve field collection item

--- themes/custom/fki/templates/field-collection/field-collection-item--field_os2web_paragraphs.tpl.php
+++ themes/custom/fki/templates/field-collection/field-collection-item--field_os2web_paragraphs.tpl.php
@@ -1,24 +1,27 @@
-<!-- field-collection-item--field_os2web_paragraphs.tpl.php
-<!-- Begin - accordion -->
-<div class="os2-field-collection-item os2-accordion <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<?php if ($view_mode == 'full'): ?>
+  <!-- field-collection-item--field_os2web_paragraphs.tpl.php
+  <!-- Begin - accordion -->
+  <div class="os2-field-collection-item os2-accordion <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <?php if (isset($content['field_os2web_paragraph_title'])): ?>
-    <!-- Begin - heading -->
-    <div class="os2-accordion-heading">
-      <div class="os2-accordion-heading-title">
-        <?php print render($content['field_os2web_paragraph_title']); ?>
+    <?php if (isset($content['field_os2web_paragraph_title'])): ?>
+      <!-- Begin - heading -->
+      <div class="os2-accordion-heading">
+        <div class="os2-accordion-heading-title">
+          <?php print render($content['field_os2web_paragraph_title']); ?>
+        </div>
       </div>
-    </div>
-    <!-- End - heading -->
-  <?php endif; ?>
+      <!-- End - heading -->
+    <?php endif; ?>
 
-  <?php if (isset($content['field_os2web_paragraph_text'])): ?>
-    <!-- Begin - body -->
-    <div class="os2-accordion-body">
-      <?php print render($content['field_os2web_paragraph_text']); ?>
-    </div>
-    <!-- End - body -->
-  <?php endif; ?>
+    <?php if (isset($content['field_os2web_paragraph_text'])): ?>
+      <!-- Begin - body -->
+      <div class="os2-accordion-body">
+        <?php print render($content['field_os2web_paragraph_text']); ?>
+      </div>
+      <!-- End - body -->
+    <?php endif; ?>
 
-</div>
-<!-- End - - accordion -->
+  </div>
+  <!-- End - - accordion -->
+
+<?php endif; ?>
