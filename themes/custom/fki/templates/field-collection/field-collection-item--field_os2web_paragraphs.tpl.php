9964158d17f285963b049d04333cea074a33be45
Morten Nissen <mn@bellcom.dk>
2016-05-29 13:49:10 +0200

    Accordion on content pages

---
+++ themes/custom/fki/templates/field-collection/field-collection-item--field_os2web_paragraphs.tpl.php
@@ -0,0 +1,24 @@
+<!-- field-collection-item--field_os2web_paragraphs.tpl.php
+<!-- Begin - accordion -->
+<div class="os2-field-collection-item os2-accordion <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php if (isset($content['field_os2web_paragraph_title'])): ?>
+    <!-- Begin - heading -->
+    <div class="os2-accordion-heading">
+      <div class="os2-accordion-heading-title">
+        <?php print render($content['field_os2web_paragraph_title']); ?>
+      </div>
+    </div>
+    <!-- End - heading -->
+  <?php endif; ?>
+
+  <?php if (isset($content['field_os2web_paragraph_text'])): ?>
+    <!-- Begin - body -->
+    <div class="os2-accordion-body">
+      <?php print render($content['field_os2web_paragraph_text']); ?>
+    </div>
+    <!-- End - body -->
+  <?php endif; ?>
+
+</div>
+<!-- End - - accordion -->
