22d1dff56c559d6e92d5cf1122b1fd1d17ef4b00
Morten Nissen <mn@bellcom.dk>
2016-06-16 13:25:00 +0200

    Feedback form

--- themes/custom/fki/templates/custom/feedback-form-display.tpl.php
+++ themes/custom/fki/templates/custom/feedback-form-display.tpl.php
@@ -1,7 +1,7 @@
 <!-- feedback-form-display.tpl.php -->
 <!-- Begin - feedback -->
 <div class="popover-button popover-button-feedback" id="block-feedback-form">
-  <a href="#" class="popover-button-toggle"><?php print $title; ?></a>
+  <a href="#" class="popover-button-toggle"><span class="feedback-link"><?php print $title; ?></span></a>
 
   <div class="popover-button-content">
     <?php print render($content); ?>
