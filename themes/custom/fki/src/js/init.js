cd5ab368d0dd24d5f727a3478516b5853b45d344
Yani0029 <yani@bellcom.dk>
2016-06-30 01:53:45 +0200

    FKDK-556 news page

--- themes/custom/fki/src/js/init.js
+++ themes/custom/fki/src/js/init.js
@@ -3,21 +3,24 @@
   'use strict';
 
   // Enable BS3 designer
-  bs3Designer.init();
+  //bs3Designer.init();
 
   // Enable BS3 sidebar
-  bs3Sidebar.init();
+  //bs3Sidebar.init();
 
   // Enable popover button
   popoverButton.init();
 
   // Self-service toggle
-  equalHeight.init();
+  //equalHeight.init();
 
   // Self-service toggle
   os2SelfServiceToggle.init();
 
   // Accordion
   os2Accordion.init();
+
+  // News
+  newsIsotoper.init();
 
 })(jQuery);
