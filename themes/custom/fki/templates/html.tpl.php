885ae43b69be94fd3efc00999197364aaa343484
riivobellcom <riivo.erik@bellcom.ee>
2017-02-06 09:54:28 +0200

    Added Siteimprove Analytics code

--- themes/custom/fki/templates/html.tpl.php
+++ themes/custom/fki/templates/html.tpl.php
@@ -60,5 +60,16 @@
 
 <?php print $page_bottom; ?>
 
+<!-- Begin - Siteimprove Analytics code //-->
+<script type="text/javascript">
+/*<![CDATA[*/
+(function() {
+var sz = document.createElement('script'); sz.type = 'text/javascript'; sz.async = true;
+sz.src = '//siteimproveanalytics.com/js/siteanalyze_133884.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sz, s);
+})();
+/*]]>*/
+</script>
+<!-- End - Siteimprove Analytics code //-->
 </body>
 </html>
