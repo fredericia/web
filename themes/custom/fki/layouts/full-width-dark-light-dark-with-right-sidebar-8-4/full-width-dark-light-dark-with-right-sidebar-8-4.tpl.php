1b851a80a8c582c4e57d984c67da7d69c6d78382
Franz Skaaning <fs@bellcom.dk>
2017-03-07 07:45:09 +0100

    class på content area

--- themes/custom/fki/layouts/full-width-dark-light-dark-with-right-sidebar-8-4/full-width-dark-light-dark-with-right-sidebar-8-4.tpl.php
+++ themes/custom/fki/layouts/full-width-dark-light-dark-with-right-sidebar-8-4/full-width-dark-light-dark-with-right-sidebar-8-4.tpl.php
@@ -1,4 +1,4 @@
-<!-- full-width-dark-light-dark-with-right-sidebar-9-3.tpl.php -->
+<!-- full-width-dark-light-dark-with-right-sidebar-8-4.tpl.php -->
 <div <?php if (!empty($css_id)) {
     echo "id=\"$css_id\"";
 } ?>>
@@ -19,20 +19,20 @@
 
   <?php if ($content['content'] or $content['sidebar']): ?>
     <!-- Begin - dark section no. 1 -->
-    <div class="os2-section os2-section-dark os2-watermark os2-watermark-light">
-      <div class="container">
+    <div class="os2-section os2-section-dark os2-watermark os2-watermark-light content-primary">
+      <div class="container content-primary">
         <div class="row">
 
           <?php if ($content['sidebar']): ?>
 
             <!-- Begin - content -->
-            <div class="col-sm-8">
+            <div class="col-sm-8 content-content">
               <?php echo $content['content']; ?>
             </div>
             <!-- End - content -->
 
             <!-- Begin - sidebar -->
-            <div class="col-sm-4 hidden-print">
+            <div class="col-sm-4 hidden-print content-sidebar">
                 <?php echo $content['sidebar']; ?>
             </div>
             <!-- End - sidebar -->
@@ -40,7 +40,7 @@
           <?php else: ?>
 
             <!-- Begin - content -->
-            <div class="col-xs-12">
+            <div class="col-xs-12 content-content">
                 <?php echo $content['content']; ?>
             </div>
             <!-- End - content -->
@@ -58,7 +58,7 @@
     <div class="os2-section os2-section-light">
       <div class="container">
         <div class="row">
-          <div class="col-xs-12">
+          <div class="col-xs-12 content-light-section-1">
               <?php echo $content['light_section_1']; ?>
           </div>
         </div>
@@ -68,17 +68,17 @@
   <?php endif; ?>
 
   <?php if ($content['dark_section_1']): ?>
-    <!-- Begin - dark section no. 2 -->
+    <!-- Begin - dark section no. 1 -->
     <div class="os2-section os2-section-dark">
       <div class="container">
         <div class="row">
-          <div class="col-xs-12">
+          <div class="col-xs-12 content-dark-section-1">
               <?php echo $content['dark_section_1']; ?>
           </div>
         </div>
       </div>
     </div>
-    <!-- End - dark section no. 2 -->
+    <!-- End - dark section no. 1 -->
   <?php endif; ?>
 
 </div>
