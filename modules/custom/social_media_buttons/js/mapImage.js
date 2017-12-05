a19182c45caebacceb6365432a603397462ea418
Andriy Iun <andriy.yun@gmail.com>
2017-12-05 15:04:14 +0100

    FKDK-cleanup Moving contrib and common custom modules to profile https://github.com/bellcom/os2web_profile/pull/6

--- modules/custom/social_media_buttons/js/mapImage.js
+++
@@ -1,8 +0,0 @@
-//Getting the path of the first image in the content.
-jQuery(document).ready(function($){
-   var imgUrl = $('.content').find('img').attr('src');
-   $("meta[property='og:image']").attr("content", imgUrl);    
-});
-
-
-
