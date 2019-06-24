98c47d0dc1b94804ab24964557a59e70676ac099
Morten Nissen <mail@hjemmesidekongen.dk>
2019-06-24 10:14:58 +0200

    WCAG language

--- themes/custom/fki/templates/html.tpl.php
+++ themes/custom/fki/templates/html.tpl.php
@@ -1,9 +1,41 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]><html class="lt-ie10 lt-ie9 lt-ie8 lt-ie7 no-js" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>><![endif]-->
-<!--[if IE 7]><html class="lt-ie10 lt-ie9 lt-ie8 ie7 no-js" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>><![endif]-->
-<!--[if IE 8]><html class="lt-ie10 lt-ie9 ie8 no-js" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>><![endif]-->
-<!--[if IE 9]><html class="lt-ie10 ie9 no-js" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>><![endif]-->
-<!--[if (gt IE 9)|!(IE)]><html class="not-ie no-js" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?> xmlns="http://www.w3.org/1999/html"><!--<![endif]-->
+<!--[if lt IE 7]>
+<html
+  class="lt-ie10 lt-ie9 lt-ie8 lt-ie7 no-js"
+  lang="<?php print $language->language; ?>"
+  dir="<?php print $language->dir; ?>"
+  <?php print $rdf_namespaces; ?>
+>
+<![endif]-->
+<!--[if IE 7]>
+<html
+  class="lt-ie10 lt-ie9 lt-ie8 ie7 no-js"
+  lang="<?php print $language->language; ?>"
+  dir="<?php print $language->dir; ?>"
+  <?php print $rdf_namespaces; ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html
+  class="lt-ie10 lt-ie9 ie8 no-js"
+  lang="<?php print $language->language; ?>"
+  dir="<?php print $language->dir; ?>"
+  <?php print $rdf_namespaces; ?>>
+<![endif]-->
+<!--[if IE 9]>
+<html
+  class="lt-ie10 ie9 no-js"
+  lang="<?php print $language->language; ?>"
+  dir="<?php print $language->dir; ?>"
+  <?php print $rdf_namespaces; ?>>
+<![endif]-->
+<!--[if (gt IE 9)|!(IE)]>
+<html
+  class="not-ie no-js"
+  lang="<?php print $language->language; ?>"
+  dir="<?php print $language->dir; ?>"
+  <?php print $rdf_namespaces; ?>
+>
+<!--<![endif]-->
 <head>
 
   <title><?php print $head_title; ?></title>
