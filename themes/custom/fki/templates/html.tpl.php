bde7b1e5bc10bd3a73116c98f624ae08bb5b5c9c
Morten Nissen <mail@hjemmesidekongen.dk>
2021-08-27 08:59:48 +0200

    KGJ-530-69306

--- themes/custom/fki/templates/html.tpl.php
+++ themes/custom/fki/templates/html.tpl.php
@@ -40,8 +40,8 @@
 
   <title><?php print $head_title; ?></title>
   <meta http-equiv="content-language" content="da,en">
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
   <?php print $head; ?>
